9c85738ed3801b3316c968a1d00fbeaf5216f2c7
ALLIANCE TRAVEL <alliancetravel34@gmail.com>
2026-04-12 13:41:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3860090338-4fad0",
+  "appId": "1:808554746516:web:7e9f664e4f3ce9e6e49a81",
+  "apiKey": "AIzaSyDTqv7fCIaYmeKkea1Lvm75Z3_otFPQxdM",
+  "authDomain": "studio-3860090338-4fad0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "808554746516"
+};
